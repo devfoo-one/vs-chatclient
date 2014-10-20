a27b9521acb111c4036105e0c4ee8a1a00f2dcf2
Tom Oberhauser <tom@devfoo.de>
2014-10-20 13:27:51 +0200

    initial commit nr. 2

---
+++ src/client/client.java
@@ -0,0 +1,10 @@
+package client;
+
+/**
+ * client class
+ */
+public class client {
+    public static void main(final String[] args) {
+        System.out.println("client...");
+    }
+}
---
+++ src/server/server.java
@@ -0,0 +1,10 @@
+package server;
+
+/**
+ * server class
+ */
+public class server {
+    public static void main(final String[] args) {
+        System.out.println("server...");
+    }
+}
